e13b500fcc4647aa888534757f7015709254d655
Cheuky Li <lich25@oocl.com>
2025-09-04 20:00:48 +0800

    refactor: replace Input class with record, remove Input class

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,31 +8,33 @@ public class WordFrequencyGame {
 
     public static final String ANY_SPACE_SEPARATOR = "\\s+";
 
+    public record WordCount(String word, int count) {}
+
     public String getResult(String inputStr) {
         String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
         try {
-            List<Input> frequencies = countFrequencies(words);
-            frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+            List<WordCount> frequencies = countFrequencies(words);
+            frequencies.sort((w1, w2) -> w2.count - w1.count);
             return composeOutput(frequencies);
         } catch (Exception e) {
             return "Calculate Error";
         }
     }
 
-    private static String composeOutput(List<Input> frequencies) {
+    private static String composeOutput(List<WordCount> frequencies) {
         StringJoiner joiner = new StringJoiner("\n");
-        for (Input w : frequencies) {
-            String s = w.getValue() + " " + w.getWordCount();
+        for (WordCount w : frequencies) {
+            String s = w.word + " " + w.count;
             joiner.add(s);
         }
         return joiner.toString();
     }
 
-    private List<Input> countFrequencies(String[] words) {
+    private List<WordCount> countFrequencies(String[] words) {
         Map<String, Integer> groups = groupSameWords(words);
-        List<Input> frequencies = new ArrayList<>();
+        List<WordCount> frequencies = new ArrayList<>();
         for (Map.Entry<String, Integer> entry : groups.entrySet()) {
-            frequencies.add(new Input(entry.getKey(), entry.getValue()));
+            frequencies.add(new WordCount(entry.getKey(), entry.getValue()));
         }
         return frequencies;
     }
