db82d8b9ff91aaa8ac243bb0ea669d11df65f2da
Cheuky Li <lich25@oocl.com>
2025-09-04 17:35:08 +0800

    refactor: remove duplicate code of inputStr.split(ANY_SPACE_SEPARATOR)

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -9,26 +9,23 @@ public class WordFrequencyGame {
     public static final String ANY_SPACE_SEPARATOR = "\\s+";
 
     public String getResult(String inputStr) {
-        if (inputStr.split(ANY_SPACE_SEPARATOR).length == 1) {
+        String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
+        if (words.length == 1) {
             return inputStr + " 1";
-        } else {
-            try {
-                //split the input string with 1 to n pieces of spaces
-                String[] words = inputStr.split(ANY_SPACE_SEPARATOR);
+        }
+        try {
+            List<Input> frequencies = countFrequencies(words);
 
-                List<Input> frequencies = countFrequencies(words);
+            frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
-                frequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : frequencies) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Input w : frequencies) {
+                String s = w.getValue() + " " + w.getWordCount();
+                joiner.add(s);
             }
+            return joiner.toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
     }
 
